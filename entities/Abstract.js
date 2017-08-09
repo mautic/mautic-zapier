b191f1c51ef07a2d1857a81f1abdd13da0b324a6
John Linhart <admin@escope.cz>
2017-08-09 13:09:31 +0200

    Throw error on 404 when fetching items

--- entities/Abstract.js
+++ entities/Abstract.js
@@ -10,7 +10,12 @@ Abstract = function(z, bundle, route, singular, plural) {
   };
 
   this.getItem = (id) => {
-    return z.request({url: route+'/'+id}).then((response) => JSON.parse(response.content)[singular]);
+    return z.request({url: route+'/'+id}).then((response) => {
+      if (response.status === 404) {
+        throw new Error(singular+' with ID '+bundle.inputData.formId+' not found');
+      }
+      return JSON.parse(response.content)[singular];
+    });
   };
 
 };
