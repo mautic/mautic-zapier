894e1579a969c970fc7623e8302ba6f6aba1fab6
John Linhart <admin@escope.cz>
2018-02-22 15:34:34 +0100

    CS fixes

--- entities/Abstract.js
+++ entities/Abstract.js
@@ -1,23 +1,21 @@
 Abstract = function(z, bundle, route, singular, plural) {
-
-  this.getList = (params) => {
+  this.getList = params => {
     const options = {
       url: route,
       params: params,
     };
 
-    return z.request(options).then((response) => JSON.parse(response.content)[plural]);
+    return z.request(options).then(response => JSON.parse(response.content)[plural]);
   };
 
   this.getItem = (id) => {
-    return z.request({url: route+'/'+id}).then((response) => {
+    return z.request({url: route+'/'+id}).then(response => {
       if (response.status === 404) {
         throw new Error(singular+' with ID '+bundle.inputData.formId+' not found');
       }
       return JSON.parse(response.content)[singular];
     });
   };
-
 };
 
 module.exports = Abstract;
