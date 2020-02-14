d72b76910d8760ce45f904e6f1e399d8dff084e2
Zdeno Kuzmany <zdeno@kuzmany.biz>
2020-02-14 10:08:51 +0100

    Improve comments

--- authentication.js
+++ authentication.js
@@ -15,7 +15,7 @@ const getAccessToken = (z, bundle) => {
     }
   });
 
-  // Needs to return at minimum, `access_token`, and if your app also does refresh, then `refresh_token` too
+  // Needs to return  `access_token` and refresh_token
   return promise.then(response => {
     if (response.status !== 200) {
       throw new Error('Unable to fetch access token: ' + response.content);
@@ -28,22 +28,22 @@ const getAccessToken = (z, bundle) => {
     };
   });
 };
+
 const refreshAccessToken = (z, bundle) => {
-  const promise = z.request(bundle.authData.baseUrl+'/oauth/v2/token', {
+  const promise = z.request(bundle.inputData.baseUrl+'/oauth/v2/token', {
     method: 'POST',
     body: {
       refresh_token: bundle.authData.refresh_token,
-      client_id: bundle.authData.clientId,
-      client_secret: bundle.authData.clientSecret,
-      grant_type: 'refresh_token',
+      client_id: bundle.inputData.clientId,
+      client_secret: bundle.inputData.clientSecret,
+      grant_type: 'refresh_token'
     },
     headers: {
       'content-type': 'application/x-www-form-urlencoded'
     }
   });
 
-  // Needs to return `access_token`. If the refresh token stays constant, can skip it. If it changes, can
-  // return it here to update the user's auth on Zapier.
+  // Needs to return  `access_token` and refresh_token
   return promise.then(response => {
     if (response.status !== 200) {
       throw new Error('Unable to fetch access token: ' + response.content);
@@ -51,19 +51,18 @@ const refreshAccessToken = (z, bundle) => {
 
     const result = JSON.parse(response.content);
     return {
-      access_token: result.access_token,
-      refresh_token: result.refresh_token
+      access_token: result.access_token
     };
   });
 };
 
 
 const testAuth = (z , bundle ) => {
-  // Normally you want to make a request to an endpoint that is either specifically designed to test auth, or one that
-  // every user will have access to, such as an account or profile endpoint like /me.
+  // Normally you want to make a request to an endpoint that is either specifically designed to test auth, or one that every user will have access to, such as an account or profile endpoint like /me.
   const promise = z.request({
     method: 'GET',
     url: bundle.authData.baseUrl+'/api/contacts?limit=1&minimal=1',
+    headers: { Authorization: 'Bearer '+bundle.authData.access_token },
   });
 
   // This method can return any truthy value to indicate the credentials are valid.
@@ -87,9 +86,8 @@ module.exports = {
     {key: 'baseUrl', type: 'string', required: true, helpText: 'The root URL of your Mautic installation starting with https://. E.g. https://my.mautic.net.'}
   ],
   oauth2Config: {
-    // Step 1 of the OAuth flow; specify where to send the user to authenticate with your API.
+    // Step 1 of the OAuth flow; specify where to send the user to authenticate with Mautic API.
     // Zapier generates the state and redirect_uri, you are responsible for providing the rest.
-    // Note: can also be a function that returns a string
     authorizeUrl: {
       url: '{{bundle.inputData.baseUrl}}/oauth/v2/authorize',
       params: {
@@ -102,18 +100,13 @@ module.exports = {
     // Step 2 of the OAuth flow; Exchange a code for an access token.
     // This could also use the request shorthand.
     getAccessToken: getAccessToken,
-    // (Optional) If the access token expires after a pre-defined amount of time, you can implement
-    // this method to tell Zapier how to refresh it.
+    // The access token expires after a pre-defined amount of time, these method refresh it
     refreshAccessToken: refreshAccessToken,
-    // If you want Zapier to automatically invoke `refreshAccessToken` on a 401 response, set to true
+    // Zapier to automatically invoke `refreshAccessToken` on a 401 response
     autoRefresh: true
-    // If there is a specific scope you want to limit your Zapier app to, you can define it here.
-    // Will get passed along to the authorizeUrl
-    // scope: 'read,write'
   },
-  // The test method allows Zapier to verify that the access token is valid. We'll execute this
-  // method after the OAuth flow is complete to ensure everything is setup properly.
+  // The test method allows Zapier to verify that the access token is valid
   test: testAuth,
-  // assuming "username" is a key returned from the test
+  // assuming "baseUrl of Mautic"
   connectionLabel: '{{baseUrl}}',
 };
