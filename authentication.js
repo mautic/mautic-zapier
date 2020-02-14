9a07a10249211a1c6eace31061696c3fac0662a5
Zdeno Kuzmany <zdeno@kuzmany.biz>
2020-02-14 10:19:20 +0100

    Change test hook

--- authentication.js
+++ authentication.js
@@ -61,8 +61,7 @@ const testAuth = (z , bundle ) => {
   // Normally you want to make a request to an endpoint that is either specifically designed to test auth, or one that every user will have access to, such as an account or profile endpoint like /me.
   const promise = z.request({
     method: 'GET',
-    url: bundle.authData.baseUrl+'/api/contacts?limit=1&minimal=1',
-    headers: { Authorization: 'Bearer '+bundle.authData.access_token },
+    url: bundle.authData.baseUrl+'/api/contacts/list/fields',
   });
 
   // This method can return any truthy value to indicate the credentials are valid.
